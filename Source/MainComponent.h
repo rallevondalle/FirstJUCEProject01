9f9548c9a116c9935b82be53bd9453a8aa0358bd
Paulo Chiliguano <paulo.chiliguano@gmail.com>
2020-05-25 00:56:48 -0500

    Instantiate a ScaleTableComponent object

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -34,7 +34,7 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
-
+    ScaleTableComponent scaleTable;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
